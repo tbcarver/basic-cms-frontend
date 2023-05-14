ddab3631a95b546649669b9acdc43738dbe567f6
Tyler Carver <tbcarver@yahoo.com>
2023-05-14 09:06:10 -0600

    Added daisy ui.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          "primary": "#261426",
+          "secondary": "#D9C589",
+          "accent": "#D9843B",
+          "neutral": "#F8E29C",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 } satisfies Config;

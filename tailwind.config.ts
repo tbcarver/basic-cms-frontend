05c6a388bb1ff482f7a270942a75c89969998e13
Tyler Carver <tbcarver@yahoo.com>
2023-05-15 09:22:02 -0600

    Adding a build step to create a purge white list.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
